7db16c430cc7d7d104f9b0578bb77b061da73545
Tatyana <42807236+Tatyana-Pogrebnyak@users.noreply.github.com>
2020-09-06 21:29:27 +0300

    write Salary calculator

--- src/chapter3a/SalaryCalculator.java
+++ src/chapter3a/SalaryCalculator.java
@@ -1,4 +1,27 @@
 package chapter3a;
+import java.util.Scanner;
+/*
+IF statement.
+All Salespeople get a salary of $1000 per week.
+Those who made more that 10 sales get an additional bonus of $250. Let's code this.
+ */
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-public class SalaryCakculator {
+        //1. Get what we know
+        int salary = 1000;
+        int bonus = 250;
+
+        //2.Get what we dont know
+        System.out.println("Enter a number of sales employee made this week:");
+        int numberOfSales = scan.nextInt();
+
+        //3. Calculate the result
+        if(numberOfSales > 10) {
+            salary = salary + bonus;
+        }
+
+        System.out.println("Employee's salary is: $" + salary);
+    }
 }
