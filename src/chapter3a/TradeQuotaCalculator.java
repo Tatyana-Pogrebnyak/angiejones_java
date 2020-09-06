da5633f8b2578b1991644ef76c7343ecceee3895
Tatyana <42807236+Tatyana-Pogrebnyak@users.noreply.github.com>
2020-09-06 22:26:57 +0300

    implement TradeQuotaCalculator

---
+++ src/chapter3a/TradeQuotaCalculator.java
@@ -0,0 +1,34 @@
+package chapter3a;
+import java.util.Scanner;
+
+/*
+IF-ELSE Statement.
+All salespeople are expected to make at least 10 sales each week.
+Those who do, they receive a congradulatory message.
+Those who dont, they receive a message about amount of missed sales. Let's code this.
+ */
+
+public class TradeQuotaCalculator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //1. Get what we know
+        int mandatorySales = 10;
+        String congradulatoryMessage = "Congrats!! You made good sales this week!";
+        String missedSalesMessage = "So sad, you have missed this amount of sales: ";
+
+        //2. Get what we dont know
+        System.out.println("Enter the number of sales made this week:");
+        int actualSales = scan.nextInt();
+
+        //3. Calculate the result
+        if(actualSales >= mandatorySales) {
+            System.out.println(congradulatoryMessage);
+        } else {
+            System.out.println(missedSalesMessage + (mandatorySales - actualSales));
+        }
+
+
+    }
+}
