ffa8f44abe73b6ef12b5bc02934516aee0ed74d7
Tatyana Pogrebnyak <tatyana.pogrebnyak@nordigy.ru>
2020-09-09 07:58:41 +0300

    update template class for Rectangles

--- src/chapter6/Rectangle.java
+++ src/chapter6/Rectangle.java
@@ -2,24 +2,24 @@ package chapter6;
 
 public class Rectangle {
 
+    private double length;
+    private double width;
+
+    //constructors
     public Rectangle(){
         length = 10;
         width = 15;
     }
-
     public Rectangle(double length, double width){
         //this.length = length; //Can be set this way,
         setLength(length);      // or can ve set this way.
         setWidth(width);
     }
 
-    double length;
-    double width;
-
-    double  calculatePerimeter(){
+    //methods
+    double calculatePerimeter(){
         return ((2*length) + (2*width));
     }
-
     double calculateArea(){
         return length * width;
     }
@@ -28,7 +28,6 @@ public class Rectangle {
     void setLength(double length){
         this.length = length;
     }
-
     void setWidth (double width){
         this.width = width;
     }
@@ -37,7 +36,6 @@ public class Rectangle {
     double getLength(){
         return length;
     }
-
     double getWidth(){
         return width;
     }
