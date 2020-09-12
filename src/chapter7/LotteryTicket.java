6c8eb4bdab0f347f3ff577a814239a8164ec9fc1
Tatyana Pogrebnyak <tatyana.pogrebnyak@nordigy.ru>
2020-09-12 19:34:10 +0300

    LotteryTicket.java

---
+++ src/chapter7/LotteryTicket.java
@@ -0,0 +1,17 @@
+package chapter7;
+
+public class LotteryTicket {
+
+    private static final int LENGTH = 6;
+
+    public static void main(String[] args) {
+
+    }
+
+    //Write a method that will generate a randon array of numbers:
+
+    public static int[] generateNumbers(){
+        int[] lotteryTicket = new int[LENGTH];
+
+    }
+}
